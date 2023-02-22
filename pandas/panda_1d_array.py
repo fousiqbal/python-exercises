fb98aa685ac4947628d65020f21b7c42c1e79445
fousiqbal <fousiqbal2@gmail.com>
2023-02-22 09:34:55 +0530

    pandas

---
+++ pandas/panda_1d_array.py
@@ -0,0 +1,10 @@
+#Write a Pandas program to convert a Panda module Series to Python list and it’s type.
+
+import pandas as pd
+ds = pd.Series([2, 4, 6, 8, 10])
+print("Pandas Series and type")
+print(ds)
+print(type(ds))
+print("Convert Pandas Series to Python list")
+print(ds.tolist())
+print(type(ds.tolist()))
