b8e0c928a5c404f02e9c54f55f2e1dbf1eeaa51f
fousiqbal <fousiqbal2@gmail.com>
2023-02-22 09:10:32 +0530

    exercises

--- argparse.py
+++
@@ -1,6 +0,0 @@
-import argparse
-parser.add_argument('integers', metavar ='N', type = int, nargs ='+',help ='an integer for the accumulator') 
-parser.add_argument(dest ='accumulate', action ='store_const',const = sum,  help ='sum the integers')
-args = parser.parse_args()
-print(args.accumulate(args.integers))
- 
