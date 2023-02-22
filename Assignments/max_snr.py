6c6be604d3cbe28f5b2bab944ed2a773e58dfb69
fousiqbal <120647715+fousiqbal@users.noreply.github.com>
2023-02-22 15:46:32 +0530

    Update max_snr.py

--- Assignments/max_snr.py
+++ Assignments/max_snr.py
@@ -1,39 +1,39 @@
 # Write a program to get the list of top 10 moving detections with maximum SNR for each scan.(flags parameter is empty for moving detections)
 
-with open("/home/fousiai/Downloads/replay-det_VP160.txt",'r') as file:
+with open("/home/fousiai/Downloads/replay-det_VP160.txt",'r') as file:  #read the file
 
     snr_value =[] 
     t = []
 
     for line in file:
 #         print(line)
-        x = line.find('SS')
+        x = line.find('SS')  #to remove ss 
 #         print(x)
         if x ==-1:
             list_of_words = line.split()
-            if( len( list_of_words) < 8):
+            if( len( list_of_words) < 8):   #seperate the first 2 lines
                 snr_value.append( t)
                 t = []
                 continue
             t.append( [
-                float(list_of_words[1]),
-                float(list_of_words[3]),
-                float(list_of_words[5]),
-                float(list_of_words[7]),
-                float(list_of_words[9]),
-                float(list_of_words[11]) #snr
+                float(list_of_words[1]),  #rng
+                float(list_of_words[3]),  #azi
+                float(list_of_words[5]),  #elev
+                float(list_of_words[7]),  #dopl
+                float(list_of_words[9]),  #magn
+                float(list_of_words[11])  #snr
             ])
                 
 def pf( i):
     a,b,c,d,e,f = [],[],[],[],[],[]
-    for v in snr_value[i][ :10]:
+    for v in snr_value[i][ :10]:  #get 10 values
         a.append( v[0])
         b.append( v[1])
         c.append( v[2])
         d.append( v[3])
         e.append( v[4])
         f.append( v[5])
-    print( "\nScan",i," Rng", a)
+    print( "\nScan",i," Rng", a)  #printing
     print( "Scan",i, " Azi", b)
     print( "Scan",i," Elev", c)
     print( "Scan",i, " Dopl", d)
@@ -42,5 +42,5 @@ def pf( i):
     
 
 for i in range( 1, 20):
-    snr_value[i].sort(reverse=1, key=lambda a: a[5])
+    snr_value[i].sort(reverse=1, key=lambda a: a[5])  #sort in descending order
     pf( i)
