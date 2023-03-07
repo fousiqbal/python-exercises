5c7b591c37558393701e0e5e48a8688ceeb9efc3
fousiqbal <fousiqbal2@gmail.com>
2023-03-07 14:42:51 +0530

    ddlog

---
+++ Assignments/ddlog_presets.py
@@ -0,0 +1,60 @@
+def get_usage():
+    with open(
+        "/home/fousiai/Downloads/DDR_Usage_log.txt", "r"
+    ) as file:  # reads the file
+        data = file.readlines()
+        file.close()
+    values = {}
+    usageDict = {}
+    ref = {}
+
+    for line in data:
+        if "available memory" in line:
+            line = line.replace("(", " ").replace(")", " ")
+            list_of_words = line.split()
+            usage = (
+                float(list_of_words[10])
+                * 100
+                / (float(list_of_words[5]) + float(list_of_words[10]))  # given equation
+            )
+            values[list_of_words[1]] = usage
+
+        elif ("python command" in line) and ("capture" in line):  # identify presets
+            list_of_words = line.split()
+            if "--preset" in line:
+                presets = list_of_words[5]
+            elif "mukna" in line:
+                presets = "mukna"
+            else:
+                presets = "atlas"
+            usageDict[presets] = values
+            for val in values:
+                ref[val] = {}
+            values = {}
+    for pres in usageDict:
+        for usage in usageDict[pres]:
+            ref[usage][pres] = usageDict[pres][usage]
+    return ref
+
+
+def show_usage_error(usage):  # identify error
+    for section in usage:
+        error = False
+        error_dict = {}
+        for preset in usage[section]:
+            if usage[section][preset] > 70:
+                error = True
+                error_dict[preset] = usage[section][preset]
+        if error == True:
+            print(f"JENKINS_ERROR: memory usage for {section} is >70% for")  # printing
+            for err in error_dict:
+                print(f"usage for {err} is {str(error_dict[err])} %")
+
+        else:
+            print(
+                f"JENKINS_OK: memory for {section} is within the limit for all types of scans"
+            )
+
+
+usage = get_usage()
+show_usage_error(usage)
