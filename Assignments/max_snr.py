3827b0e55b617eb38b06499f0523fc5e29dfc6f8
fousiqbal <120647715+fousiqbal@users.noreply.github.com>
2023-03-10 09:57:11 +0530

    Update max_snr.py

--- Assignments/max_snr.py
+++ Assignments/max_snr.py
@@ -1,46 +1,49 @@
 # Write a program to get the list of top 10 moving detections with maximum SNR for each scan.(flags parameter is empty for moving detections)
 
-with open("/home/fousiai/Downloads/replay-det_VP160.txt",'r') as file:  #read the file
-
-    snr_value =[] 
-    t = []
-
+  
+    with open("/home/fousiai/Downloads/replay-det_VP160.txt",'r') as file:  #read the file
+    value =[] 
+    detection = []
     for line in file:
-#         print(line)
-        x = line.find('SS')  #to remove ss 
-#         print(x)
+        x = line.find('SS') #to remove ss 
         if x ==-1:
             list_of_words = line.split()
+            
             if( len( list_of_words) < 10):   #seperate the first 2 lines
-                snr_value.append( t)
-                t = []
-                continue
-            t.append( [
-                float(list_of_words[1]),  #rng
-                float(list_of_words[3]),  #azi
-                float(list_of_words[5]),  #elev
-                float(list_of_words[7]),  #dopl
-                float(list_of_words[9]),  #magn
-                float(list_of_words[11])  #snr
-            ])
+                value.append( detection)
+                detection = []
+
+            else:
+                detection.append( [
+                    float(list_of_words[1]), #rng
+                    float(list_of_words[3]), #azi
+                    float(list_of_words[5]), #elev
+                    float(list_of_words[7]), #dopl
+                    float(list_of_words[9]),#magn
+                    float(list_of_words[11]) #snr
+                ])
                 
-def pf( i):
-    a,b,c,d,e,f = [],[],[],[],[],[]
-    for v in snr_value[i][ :10]:  #get 10 values
-        a.append( v[0])
-        b.append( v[1])
-        c.append( v[2])
-        d.append( v[3])
-        e.append( v[4])
-        f.append( v[5])
-    print( "\nScan",i," Rng", a)  #printing
-    print( "Scan",i, " Azi", b)
-    print( "Scan",i," Elev", c)
-    print( "Scan",i, " Dopl", d)
-    print( "Scan",i," Magn", e)
-    print( "Scan",i, " SNR", f)
-    
+def display_detection( i):
+    rng,azi,elev,dopl,magn,snr = [],[],[],[],[],[]
+    for v in value[i][ :10]:#get 10 values
+        rng.append( v[0])
+        azi.append( v[1])
+        elev.append( v[2])
+        dopl.append( v[3])
+        magn.append( v[4])
+        snr.append( v[5])
+    print( "\nScan",i," Rng", rng)#printing
+    print( "Scan",i, " Azi", azi)
+    print( "Scan",i," Elev", elev)
+    print( "Scan",i, " Dopl", dopl)
+    print( "Scan",i," Magn", magn)
+    print( "Scan",i, " SNR", snr)
+
+def max_snr( a):
+    return a[5]
 
 for i in range( 1, 20):
-    snr_value[i].sort(reverse=1, key=lambda a: a[5])  #sort in descending order
-    pf( i)
+    value[i].sort(reverse=1, key=max_snr) #lamda a:a[5]
+    display_detection( i)
+    
+        
